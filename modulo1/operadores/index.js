2ad0759f4e0ef1b7cd0877bd7bdc3547c354701c
Grazielle Martins <fgagraziellemartins@hotmail.com>
2021-11-30 21:19:55 -0300

    Correção do exercício 4

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -67,17 +67,9 @@ numero2 = Number(numeroDois);
 let maior = numero1 > numero2;
 let igual = numero1 === numero2;
 let divisivel1 = numero1 % numero2 === 0;
-let divisivel2 = numero1 % numero2 !== 0;
+let divisivel2 = numero2 % numero1 === 0;
 
 console.log("O primeiro numero é maior que segundo?", maior);
 console.log("O primeiro numero é igual ao segundo?", igual);
 console.log("O primeiro numero é divisível pelo segundo?", divisivel1);
 console.log("O segundo numero é divisível pelo primeiro?", divisivel2);
-
-
-
-
-
-
-
-
