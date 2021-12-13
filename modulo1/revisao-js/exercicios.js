f3b0771c9d5c27109ee6f37bd74a83a21f984e33
Grazielle Martins <fgagraziellemartins@hotmail.com>
2021-12-13 15:56:39 -0300

    Exercício 6- Revisão JS

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -38,9 +38,16 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
-}
+  let maior = -Infinity
 
+    for (let numero of array){
+       if(numero > maior){
+         maior = numero
+        
+        }
+    }
+    return maior
+}
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
 
