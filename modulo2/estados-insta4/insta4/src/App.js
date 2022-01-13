97544c585ccb0a99ee1318bc5e44393239b3c826
Grazielle Martins <fgagraziellemartins@hotmail.com>
2022-01-13 17:03:54 -0300

    exercício de renderizar array

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,25 +10,40 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state={
+    posts: [
+      {
+        nomeUsuario: "Grazi",
+        fotoUsuario: 'https://ca.slack-edge.com/TLAVDH7C2-U02N1KQEUMR-b1bd2621fd7f-512',
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: "Arthur",
+        fotoUsuario: 'https://picsum.photos/50/51',
+        fotoPost: 'https://picsum.photos/200/151' 
+      },
+      {
+        nomeUsuario: "Levi",
+        fotoUsuario: 'https://th.bing.com/th/id/OIP.9tgqlyci6jQ0BlC9BiR9fwHaFj?pid=ImgDet&rs=1',
+        fotoPost: 'https://picsum.photos/200/152'
+      }
+    ]
+  }
   render() {
-    return (
-      <MainContainer>
+    const listaDePost = this.state.posts.map((posts) => {
+      return (
         <Post
-          nomeUsuario={'Grazi'}
-          fotoUsuario={'https://ca.slack-edge.com/TLAVDH7C2-U02N1KQEUMR-b1bd2621fd7f-512'}
-          fotoPost={'https://picsum.photos/200/150'}
+          nomeUsuario={posts.nomeUsuario}
+          fotoUsuario={posts.fotoUsuario}
+          fotoPost={posts.fotoPost} 
         />
-          <Post
-          nomeUsuario={'Arthur'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
-          <Post
-          nomeUsuario={'Levi'}
-          fotoUsuario={'https://th.bing.com/th/id/OIP.9tgqlyci6jQ0BlC9BiR9fwHaFj?pid=ImgDet&rs=1'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
-      </MainContainer>
+      );
+
+    });
+    return (
+        <MainContainer>
+          {listaDePost}
+        </MainContainer>
     );
   }
 }
