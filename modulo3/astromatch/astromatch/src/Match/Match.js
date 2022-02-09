b69ed9bc521ef2034fd0edc010d3a3c1daa18b09
Grazielle Martins <fgagraziellemartins@hotmail.com>
2022-02-09 17:06:53 -0300

    astromatch

---
+++ modulo3/astromatch/astromatch/src/Match/Match.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+
+export default function App() {
+  return (
+      <div className="App">
+        <div>
+          <h2>Olá</h2>
+        </div>
+      </div>
+  
+  );
+}
