1f9b8314c983a96eaa33ed759df2b5cba369818b
Grazielle Martins <fgagraziellemartins@hotmail.com>
2022-04-19 14:47:11 -0300

    Exercicios- HERANÇA

---
+++ modulo4/heranca-polimorfismo/src/index.ts
@@ -0,0 +1,14 @@
+//HERANÇA Exercicio 1
+
+//a) Sim, criando um método público e acessando através dele.
+//b) Apenas 1.
+
+//HERANÇA Exercicio 2
+
+//a) Apenas 1.
+//b) Apenas 1, porque Customer é uma subclasse da classe User.
+
+//HERANÇA Exercicio 3
+//a) Não porque na classe não foi criado um método para que pudesse ter acesso 
+//na subclasse
+
