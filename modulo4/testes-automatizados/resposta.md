005ca4438286dfa248ae874b4274982a6833aff1
Grazielle Martins <fgagraziellemartins@hotmail.com>
2022-05-09 16:00:41 -0300

    Testes

--- modulo4/testes-automatizados/resposta.md
+++ modulo4/testes-automatizados/resposta.md
@@ -1,2 +1,4 @@
 
+Exercicio 3
 
+c) Se pode entrar ou não de acordo com a localidade e idade foi fácil de entender, a parte do retorno deu um pouco mais de trabalhinho pra entender, mas caho que consegui assimilar 
--- modulo4/testes-automatizados/src/index.ts
+++ modulo4/testes-automatizados/src/index.ts
@@ -1,4 +1,5 @@
 import { User } from "../type/User"
+import { Casino, LOCATION, NACIONALITY, Result, Usuario } from "../type/UsereCassino";
 
 export function compras(user: User, valor: number): User | undefined {
 	if(user.saldo >= valor) {
@@ -9,3 +10,44 @@ export function compras(user: User, valor: number): User | undefined {
 	}
 	return undefined
 }
+
+export function verifyAge(casino: Casino, users: Usuario[]): Result {
+    const podeentrar: Usuario[] = [];
+    const naopodeentra: Usuario[] = [];
+  
+    for (const user of users) {
+      if (casino.location === LOCATION.EUA) {
+        if (user.age >= 21) {
+          podeentrar.push(user);
+        } else {
+          naopodeentra.push(user);
+        }
+      } else if (casino.location === LOCATION.BRAZIL) {
+        if (user.age >= 18) {
+          podeentrar.push(user);
+        } else {
+          naopodeentra.push(user);
+        }
+        break;
+      }
+    }
+  
+    return {
+      brazilians: {
+        podeentrar: podeentrar
+          .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+          .map((u) => u.name),
+        naopodeentra: naopodeentra
+          .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+          .map((u) => u.name),
+      },
+      americans: {
+        podeentrar: podeentrar
+          .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+          .map((u) => u.name),
+        naopodeentra: naopodeentra
+          .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+          .map((u) => u.name),
+      },
+    };
+  }
