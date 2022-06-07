e81d708647728956f70d9b859773154320b1d769
Grazielle Martins <fgagraziellemartins@hotmail.com>
2022-06-07 17:33:21 -0300

    Doghero

---
+++ modulo4/doghero-case/src/data/migrations.ts
@@ -0,0 +1,40 @@
+import { BaseDatabase } from "../data/BaseDatabase"
+
+export class Migrations extends BaseDatabase {
+    
+   static migrations = (
+       
+    ) => {
+        BaseDatabase.connection.raw(`
+CREATE TABLE IF NOT EXISTS DogHero ( 
+    id VARCHAR(255) PRIMARY KEY,
+    status ENUM("A FAZER", "ANDAMENTO", "FEITO") DEFAULT "A FAZER",
+    date_schedule DATE NOT NULL,
+    price FLOAT NOT NULL,
+    latitude VARCHAR(45) NOT NULL,
+    longitude VARCHAR(45) NOT NULL,
+    duration VARCHAR(45) NOT NULL,
+    date_start timestamp NOT NULL,
+    date_end timestamp NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Pets ( 
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    dogheroid VARCHAR(45) NOT NULL,
+    FOREIGN KEY (dogheroid) REFERENCES DogHero(id)
+);
+
+
+    `)
+            .then(() => console.log(
+                "Tabelas criadas!"
+            ))
+            .catch(error =>
+                console.log(error.message)
+            )
+            .finally(() => {
+                BaseDatabase.connection.destroy()
+            })
+    }
+}
+Migrations.migrations()
